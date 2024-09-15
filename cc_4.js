38f3d77953a7c24bbe43c5adcef4dbf769f66eea
sarait2 <saraitorres999@gmail.com>
2024-09-15 14:35:44 -0400

    Add files via upload

---
+++ cc_4.js
@@ -0,0 +1,11 @@
+// Task 1. Create an Inventory Array of Product Objects
+
+let inventory = [
+    {name: "cups", price: 3.00, quantity: 50, lowStockLevel: 30},
+{name: "napkins", price: 5.00, quantity: 100, lowStockLevel: 40}, 
+{name: "forks", price: 2.00, quantity: 200, lowStockLevel:50},
+{name:"spoons", price: 2.50, quantity: 150, lowStockLevel:50},
+{name:"straws", price: 1.50, quantity: 300, lowStockLevel:70},
+ ];
+ console.log(inventory);
+ // Initialize inventory with product objects
